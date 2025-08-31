e87416d855ab6aef90ea22b06e8b76031ae04abc
Gabrielly Marinho <gabrielly.soares05marinho@gmail.com>
2025-08-31 20:36:45 -0300

    Atualiza API_Flask.py com rotas iniciais e exemplo de GET/POST

--- API_Flask.py
+++ API_Flask.py
@@ -2,64 +2,27 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
-users = []
-current_id = 1 
-
-
-@app.route('/users', methods=['POST'])
-def create_user():
-    global current_id
-    data = request.json
-
-    if not data or "nome" not in data or "email" not in data:
-        return jsonify({"error": "Dados inválidos. Informe nome e email."}), 400
-
-    new_user = {
-        "id": current_id,
-        "nome": data["nome"],
-        "email": data["email"]
-    }
-    users.append(new_user)
-    current_id += 1
-
-    return jsonify(new_user), 201
+produtos = [
+    {"id": 1, "nome": "Caneta", "preco": 2.5},
+    {"id": 2, "nome": "Caderno", "preco": 15.0}
+]
 
 
-@app.route('/users', methods=['GET'])
-def get_users():
-    return jsonify(users), 200
+@app.route('/')
+def home():
+    return "Olá! Flask está funcionando."
 
 
-@app.route('/users/<int:user_id>', methods=['GET'])
-def get_user(user_id):
-    for user in users:
-        if user["id"] == user_id:
-            return jsonify(user), 200
-    return jsonify({"error": "Usuário não encontrado"}), 404
+@app.route('/produtos', methods=['GET'])
+def listar_produtos():
+    return jsonify(produtos)
 
-
-@app.route('/users/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
-    data = request.json
-    for user in users:
-        if user["id"] == user_id:
-            if "nome" in data:
-                user["nome"] = data["nome"]
-            if "email" in data:
-                user["email"] = data["email"]
-            return jsonify(user), 200
-    return jsonify({"error": "Usuário não encontrado"}), 404
-
-
-@app.route('/users/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    for user in users:
-        if user["id"] == user_id:
-            users.remove(user)
-            return jsonify({"message": "Usuário excluído com sucesso"}), 200
-    return jsonify({"error": "Usuário não encontrado"}), 404
-
+@app.route('/produtos', methods=['POST'])
+def adicionar_produto():
+    novo_produto = request.get_json()
+    produtos.append(novo_produto)
+    return jsonify({"mensagem": "Produto adicionado com sucesso!", "produto": novo_produto}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
+
